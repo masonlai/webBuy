79bad5830a7a91b9267562041ad600c008bef041
masonlai <47253466+masonlai@users.noreply.github.com>
2020-04-27 14:35:08 +0800

    Add new product method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..100).each do |i|
+  item = {
+      name: "product #{'test2'}",
+      description: "This is product #{'test2'}",
+      image_url: 'https://art.pixilart.com/2a9bd9421c07695.png',
+      price: 'Free'
+  }
+  Product.create(item)
+end
